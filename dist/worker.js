2a272308a55a0a1506fb2cce9d2f92525bcf6678
Jantje19 <Jantje19@users.noreply.github.com>
2022-07-11 19:22:54 +0200

    Updated build

--- dist/worker.js
+++ dist/worker.js
@@ -1,6 +1,5 @@
 import simpleDDP from "simpleddp";
 import { expose } from "comlink";
-import ws from "isomorphic-ws";
 import WorkerWrapper from "./wrappers/worker";
 self.addEventListener("message", (evt) => {
     if (!evt.data || !("__internal" in evt.data) || !evt.data.__internal) {
@@ -9,7 +8,7 @@ self.addEventListener("message", (evt) => {
     const { type, data } = evt.data;
     switch (type) {
         case "expose":
-            expose(new WorkerWrapper(new simpleDDP({ ...data, SocketConstructor: ws })));
+            expose(new WorkerWrapper(new simpleDDP({ ...data, SocketConstructor: WebSocket })));
             break;
         default:
             throw new Error(`Invalid message type: '${type}'`);
