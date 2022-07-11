20ec2e8868b6699393e2d13acd263c66e9d1f52d
Jantje19 <Jantje19@users.noreply.github.com>
2022-07-11 21:56:50 +0200

    Update build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,6 +2,6 @@ import { simpleDDPOptions } from "simpleddp";
 import MainThreadWorkerWrapper from "./wrappers/mainThreadWorker";
 export * from "./wrappers";
 export * from "./context";
-declare const _default: (options: Omit<simpleDDPOptions, "SocketConstructor">) => MainThreadWorkerWrapper;
+declare const _default: (options: Omit<simpleDDPOptions, "SocketConstructor">, worker?: Worker) => MainThreadWorkerWrapper;
 export default _default;
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,11 @@
 import { mainThreadWrapper } from "./wrappers";
 export * from "./wrappers";
 export * from "./context";
-export default (options) => {
-    const worker = new Worker(new URL("./worker", import.meta.url), {
-        type: "module",
-    });
-    worker.postMessage({ __internal: true, type: "expose", data: options });
-    return mainThreadWrapper(worker);
+export default (options, worker) => {
+    const finalWorker = worker ??
+        new Worker(new URL("./worker", import.meta.url), {
+            type: "module",
+        });
+    finalWorker.postMessage({ __internal: true, type: "expose", data: options });
+    return mainThreadWrapper(finalWorker);
 };
